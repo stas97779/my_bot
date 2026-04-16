bed3abc286c89258207e47fdfc555a4665f8d055
Stas <super.sasha9779@yandex.ru>
2026-04-17 00:23:18 +0500

    fix token

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 ﻿import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "твой_токен")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7412345678:AAHdqTcvCH1vGWJxfSeofSs0K5PALDsaw-j")
 ADMIN_ID = 841926956
 TARGET_GROUP_ID = -1003671521021
 
